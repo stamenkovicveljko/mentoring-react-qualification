ceec6f48d0284a870d95f363190f9f3807fbcea0
Veljko <veljko@ncoded.solutions>
2020-04-14 13:50:22 +0200

    feat(repos): remove unused files, display repos list

--- src/App.tsx
+++ src/App.tsx
@@ -1,24 +1,66 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { useEffect, useState } from "react";
+
+const githubRepoEndpoint =
+  "https://api.github.com/users/stamenkovicveljko/repos";
+
+interface IGithubRepo {
+  name: string;
+  html_url: string;
+}
 
 function App() {
+  const [repos, setRepos] = useState<IGithubRepo[]>([]);
+  const [term, setTerm] = useState<string>("");
+  const [loading, setLoading] = useState<boolean>(false);
+
+  useEffect(() => {
+    const getRepos = async () => {
+      const reqOptions = {
+        headers: {
+          Accept: "application/json",
+        },
+      };
+
+      try {
+        setLoading(true);
+        const res = await (await fetch(githubRepoEndpoint, reqOptions)).json();
+        setRepos(res);
+      } catch (er) {
+        console.error(er);
+      } finally {
+        setLoading(false);
+      }
+    };
+
+    getRepos();
+  }, []);
+
+  const handleTermOnChange = (ev: React.ChangeEvent<HTMLInputElement>) => {
+    setTerm(ev.target.value);
+  };
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <input
+        value={term}
+        placeholder="Search repos"
+        onChange={handleTermOnChange}
+      />
+      {loading ? (
+        <p>loading repositories..</p>
+      ) : (
+        <ul>
+          {repos
+            .filter(({ name }) =>
+              name.toLowerCase().includes(term.toLowerCase())
+            )
+            .map((repo) => (
+              <li key={repo.name}>
+                <a href={repo.html_url}>{repo.name}</a>
+              </li>
+            ))}
+        </ul>
+      )}
     </div>
   );
 }
